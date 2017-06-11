ff9c9826566f20d40ca50741326413c58f009030
sadhanka <sadhanka@gmail.com>
2017-06-11 07:49:23 +0300

    Create func.php

--- func.php
+++ func.php
@@ -5,7 +5,7 @@
  * Date: 31.05.2017
  * Time: 20:18
  */
-
+//filename must contain only latin letters, numbers and underscores
 function isCorrectFilename ($name) {
     if (!empty($name)) {
         if (preg_match('/[a-zA-Z0-9_]/', $name)) {
@@ -15,6 +15,7 @@ function isCorrectFilename ($name) {
     return false;
 }
 
+//get texts from files in directory 'guestbook'
 function getUserTexts ($dir) {
     $filesArray = getFilesFromDir($dir);
     $outputGuestBook = '';
@@ -26,10 +27,11 @@ function getUserTexts ($dir) {
     return $outputGuestBook;
 }
 
+// find words from array $forbidden in text
 function issetForbiddenWords($text = '', $forbidden = []) {
-    if (count($forbidden)) {
+    if (count($forbidden)) { // not empty array
         foreach ($forbidden as $fWord) {
-            if (strpos($text, $fWord) !== false) {
+            if (strpos($text, $fWord) !== false) { // looking for every word in the text
                 return true;
             }
         }
@@ -37,19 +39,25 @@ function issetForbiddenWords($text = '', $forbidden = []) {
     return false;
 }
 
+// search the largest numeric filename (pay attention - it doesn't check if directory contain only numeric file names)
 function getImgNumber ($userDir) {
     $arrayFile = getFilesFromDir($userDir);
     if (!empty($arrayFile)) {
+        // the lagest number is first
         rsort($arrayFile);
+        // get first filename from array
         $fileName = current($arrayFile);
+        // get the filename without extension
         $fileName = substr($fileName, 0, -4);
     }
+    // if directory is empty - first filename must be 0.jpg
     else {
         $fileName = 0;
     }
     return $fileName;
 }
 
+// string with markup of all images (pay attention - it doesn't check if directory contain only images) 
 function getUserPhotos ($dir, $userName = '') {
     $filesArray = getFilesFromDir($dir);
     $outputPhotoBook = '';
@@ -59,6 +67,7 @@ function getUserPhotos ($dir, $userName = '') {
     return $outputPhotoBook;
 }
 
+// return array of files without '.' and '..'
 function getFilesFromDir($dir) {
     $filesArray = scandir($dir);
     array_shift($filesArray);
